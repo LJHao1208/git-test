4649a7ab4f6e0b0252d462892afe6d6edb45890f
JiaHao <1336064523@qq.com>
2022-04-23 11:10:30 +0800

    first commit

---
+++ src/main/java/com/jiahao/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.jiahao.git;
+
+/**
+ * @author 李佳豪
+ * @create 2022-04-23 11:04
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
